23fb363316c288a57e891a94d52270be13dd8a8e
LexieHolder <lexieannholder@gmail.com>
2022-03-24 22:46:54 -0400

    finishing touches

--- server.js
+++ server.js
@@ -2,6 +2,20 @@ const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
 
+require('dotenv').config();
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  });
+}
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -14,3 +28,5 @@ app.use(routes);
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
+
+module.exports = sequelize;
